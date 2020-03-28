38376b60ad87046d69f0e2b4f790b00ad82d2ba6
Pablo Bethencourt Díaz <pbeth92@gmail.com>
2020-03-28 16:59:48 +0000

    Cambios primera parte

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-import audios from "../assets/audio/*.mp3";
-import Player from "./Player.js";
+import audios from '../assets/audio/*.mp3';
+import Player from './Player.js';
 
 const map = {};
 let i = 0;
 
 for (var key in audios) {
-	i++;
-	map[`.item-${i}`] = audios[key];
+  i++;
+  map[`.item-${i}`] = audios[key];
 }
 
 const player = new Player(map);
